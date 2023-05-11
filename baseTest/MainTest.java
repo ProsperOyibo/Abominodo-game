09ddfb8e505e6194229f58fd96853cdd879e8f43
Prosper Oyibo <nonsooyibo@hotmail.co.uk>
2023-05-11 13:16:11 +0100

    New testpackage for JUnit test case and a new test class

---
+++ baseTest/MainTest.java
@@ -0,0 +1,54 @@
+package baseTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+
+	@Test
+	void testCollateGrid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testCollateGuessGrid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPg() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testPrintGuessGrid() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testRun() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDrawDominoes() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testGecko() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	void testDrawGuesses() {
+		fail("Not yet implemented");
+	}
+
+}
--- module-info.java
+++ module-info.java
@@ -1,3 +1,6 @@
 module workshop01 {
 	requires java.desktop;
+	requires junit;
+	requires org.junit.jupiter.api;
+	requires jdk.incubator.vector;
 }
